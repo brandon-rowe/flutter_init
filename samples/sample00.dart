b7c53fa73b78f0a46389ef97e5f8554754431bf9
brandon-rowe <brandon@cyntax.org>
2020-09-30 13:41:59 -0400

    Sample Dart

---
+++ samples/sample00.dart
@@ -0,0 +1,18 @@
+void main() {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print(name);
+  print(year);
+  print(antennaDiameter);
+
+  for (var planet in flybyObjects) {
+    print(planet);
+  }
+}
